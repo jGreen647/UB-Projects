5bc90364f3f41278e2738e73eaca322a09131a9f
jGreen647 <54121162+jGreen647@users.noreply.github.com>
2019-12-10 14:40:10 -0800

    Add files via upload

---
+++ finiteElementsFinal.m
@@ -0,0 +1,51 @@
+clear all; close all;
+
+num_elem = 6;
+num_nodes = num_elem+1;
+ndof = 2 * num_nodes;
+
+tipMass = 0.01536; %kg
+E = 6.9e10; %Pa
+l = 0.160; %meters
+elemLeng = l/6;
+width = 0.009525; %meters
+thick = 0.00238; %meters
+rho = 2700; %kg/m^3
+I = (1/12)*width*thick^3;
+A = thick*width;
+
+springStiffness = (E*I)/(elemLeng^3);
+massFactor = (rho*A*elemLeng)/420;
+
+k_elem = [12 6*elemLeng -12 6*elemLeng;...
+          6*elemLeng 4*elemLeng^2 -6*elemLeng 2*elemLeng^2;...
+         -12 -6*elemLeng 12 -6*elemLeng;...
+          6*elemLeng 2*elemLeng^2 -6*elemLeng 4*elemLeng^2];
+      
+m_elem = [156 22*elemLeng 54 -13*elemLeng;...
+          22*elemLeng 4*elemLeng^2 13*elemLeng -3*elemLeng^2;...
+          54 13*elemLeng 156 -22*elemLeng;...
+         -13*elemLeng -3*elemLeng^2 -22*elemLeng 4*elemLeng^2];
+
+K = zeros(ndof,ndof);
+M = zeros(ndof,ndof);
+
+for i = 1:num_elem
+    
+   n = 2*i-1;
+   m = n+3;
+   M(n:m,n:m) = M(n:m,n:m) + m_elem;
+   K(n:m,n:m) = K(n:m,n:m) + k_elem;
+   
+end
+
+
+K = K(3:14,3:14)*springStiffness;
+M = M(3:14,3:14)*massFactor;
+
+M(end-1,end-1) = M(end-1,end-1)+tipMass;
+
+[P,V] = eig(K,M);
+
+w = sqrt(V)/(2*pi);
+
