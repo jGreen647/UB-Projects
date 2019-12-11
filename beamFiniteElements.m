0282842dfaaf9ace9730605e16213932c4e98f50
jGreen647 <54121162+jGreen647@users.noreply.github.com>
2019-12-10 16:39:49 -0800

    Update beamFiniteElements.m

--- beamFiniteElements.m
+++ beamFiniteElements.m
@@ -1,9 +1,11 @@
 clear all; close all;
 
+%FEA Parameters
 num_elem = 6;
 num_nodes = num_elem+1;
 ndof = 2 * num_nodes;
 
+%Beam Parameters
 tipMass = 0.01536; %kg
 E = 6.9e10; %Pa
 l = 0.160; %meters
@@ -17,6 +19,7 @@ A = thick*width;
 springStiffness = (E*I)/(elemLeng^3);
 massFactor = (rho*A*elemLeng)/420;
 
+%Local Mass and Stiffness Elements
 k_elem = [12 6*elemLeng -12 6*elemLeng;...
           6*elemLeng 4*elemLeng^2 -6*elemLeng 2*elemLeng^2;...
          -12 -6*elemLeng 12 -6*elemLeng;...
@@ -27,6 +30,7 @@ m_elem = [156 22*elemLeng 54 -13*elemLeng;...
           54 13*elemLeng 156 -22*elemLeng;...
          -13*elemLeng -3*elemLeng^2 -22*elemLeng 4*elemLeng^2];
 
+%Global Mass and Stiffness Matrix Assembly
 K = zeros(ndof,ndof);
 M = zeros(ndof,ndof);
 
@@ -45,6 +49,7 @@ M = M(3:14,3:14)*massFactor;
 
 M(end-1,end-1) = M(end-1,end-1)+tipMass;
 
+%Determine Natural Frequencies 
 [P,V] = eig(K,M);
 
 w = sqrt(V)/(2*pi);
